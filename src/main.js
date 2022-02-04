d923c8cf717ebb9333ff5673364fcff5cd540eda
F-hiller <jjun3511@naver.com>
2022-02-04 23:17:35 +0900

    2/4 install package and set env

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// @ts-check
+const PORT = 5000
+
+const Koa = require('koa')
+const path = require('path')
+const Pug = require('koa-pug')
+
+
+const app = new Koa()
+
+//@ts-ignore
+new Pug({
+    viewPath: path.resolve(__dirname, './views'),
+    app,
+  })
+
+
+app.use(async (ctx) => {
+    await ctx.render('main')
+})
+
+app.listen(PORT)
